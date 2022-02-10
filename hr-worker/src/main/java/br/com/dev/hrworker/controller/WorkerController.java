f4ef5f7e57a115912b35b6aad3ef9e45a50727ce
marcusviniciusls <viniciusmls@outlook.com>
2022-02-09 23:06:40 -0300

    Actuator Refresh and hr-worker client config

--- hr-worker/src/main/java/br/com/dev/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/br/com/dev/hrworker/controller/WorkerController.java
@@ -2,18 +2,35 @@ package br.com.dev.hrworker.controller;
 
 import br.com.dev.hrworker.service.WorkerService;
 import br.com.dev.hrworker.service.response.WorkerDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+@RefreshScope
 @RestController
 @RequestMapping("/worker")
 public class WorkerController {
 
     @Autowired
     private WorkerService workerService;
+
+    private static Logger LOOGGER = LoggerFactory.getLogger(WorkerController.class);
+
+    @Value("${test.config}")
+    private String testConfig;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs(){
+        LOOGGER.info("Config" + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<WorkerDto> findById(@PathVariable Long id){
